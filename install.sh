701e4ba1921f2043a57e8affca5de33e920871e8
n-wtw-n <34408461+n-wtw-n@users.noreply.github.com>
2019-12-31 16:07:13 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+(( $EUID != 0 )) && { printf '%s!\n' "This script must be run as root"; exit 1; }
+if [[ -z "$1" ]]; then
+	printf '%s\n' "USAGE: $0 \"/path/to/logs.tar.gz\""
+	exit 1
+elif [[ ! -d "${1%/*}" ]]; then
+	printf '%s\n' "USAGE: $0 \"/path/to/logs.tar.gz\""
+	exit 1
+elif [[ ! "$1" =~ ^.*'.tar.gz'$ ]]; then
+	file="$1.tar.gz"
+fi
+timer="/etc/systemd/system/move-rotated-logs.timer"
+service="/etc/systemd/system/move-rotated-logs.service"
+execf="/usr/local/bin/moverotatedlogs"
+if [[ ! -f "$execf" ]]; then
+	cp -f "${0%/*}/moverotatedlogs.sh" "$execf" >/dev/null 2>&1 || { printf '%s\n' "Could not copy move-rotated-logs.sh to ${execf%/*}."; exit 1; }
+	chmod +x "$execf" >/dev/null 2>&1 || { printf '%s\n' "Could not make move-rotated-logs.sh to executable."; exit 1; }
+fi
+if [[ ! -f "$timer" ]]; then
+	cat >"$timer" <<EOF
+[Unit]
+Description=Run post-logrotate script every day
+
+[Timer]
+OnCalendar=daily
+AccuracySec=12h
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+fi
+if [[ ! -f "$service" ]]; then
+	cat >"$service" <<EOF
+[Unit]
+Description=Backup rotated files to NAS
+
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/moverotatedlogs -f "${file-$1}"
+User=root
+EOF
+fi
+systemctl is-enabled "${timer##*/}" >/dev/null 2>&1 || systemctl enable "${timer##*/}"
+systemctl start "${timer##*/}" >/dev/null 2>&1 
+exit 0
---
+++ moverotatedlogs.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+(( $EUID == 0 )) || 
+{ 
+	printf '%s!\n' "This script must be run as root"
+	exit 1
+}
+function usage () {
+		printf '%s\n' "USAGE: $0 -f /path/to/tarfile"
+		exit 0
+}
+if (( $# >= 1 )); then
+	while (( $# )); do
+		if [[ "$1" == '-'[a-zA-Z] ]]; then
+			case "${1#-}" in
+				f)
+					tarfile="${2%/*}/$(date '+%Y-%m-%d').${2##*/}"
+					;;
+				h)
+					usage
+					;;
+				*)
+					printf '%s\n' "Unknown option: $1"
+					usage
+			esac
+		else
+			printf '%s\n' "Unknown option: $1"
+			usage
+		fi
+		shift 2
+	done
+else
+	usage
+fi
+mapfile -t logs < <(find /var/log -type f -regextype egrep -regex '.*([1-9]|[1-9].gz)')
+[[ -z "${logs[@]}" ]] && 
+{ 
+	logger "There are no rotated logs to backup to $tarfile."
+	exit 1
+}
+[[ -f "$tarfile" ]] && 
+{ 
+	gunzip "$tarfile" && tar --remove-files -rf "${tarfile%.gz}" "${logs[@]}" && gzip -9 "${tarfile%.gz}"
+	result=$? 
+} || 
+{ 
+	tar --remove-files -czf "$tarfile" "${logs[@]}"
+	result=$?
+}
+(( "$result" == 0 )) && 
+{
+	logger "Backed up rotated logs from /var/log to $tarfile."
+	exit 0
+} ||
+{
+	logger "Failed to Backup rotated logs from /var/log to $tarfile."
+	exit $result
+}
